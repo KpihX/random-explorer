f910618d1b5f99f2ba1025db6c2b7a88c3592376
Pacifique000 <ngantaangepacifique@gmail.com>
2026-01-25 23:51:21 +0100

    done 1,2,3

--- src/random_explorer/plot_environment.py
+++ src/random_explorer/plot_environment.py
@@ -1,45 +1,3 @@
-def valid_input() : 
-    with open('scenario2.txt', 'r', encoding = 'utf-8') as f:
-        count =1
-        xmax=0
-        ymax=0
-        u_s1 = []
-        u_d1 = []
-        u_s2 = []
-        u_d2 = []
-        R = 0
-        obs =[]
-        for ligne in f: 
-            s = ligne.strip()
-            p = s.split()
-            if (count <=11):
-                if len(p)!=1:
-                    return []
-                else:
-                    if count ==1:
-                        xmax=int(float(p[0]))
-                    if count ==2:
-                        ymax=int(float(p[0]))
-                    if count == 3 or count ==4:
-                        u_s1.append(int(float(p[0])))
-                    if count == 5 or count ==6:
-                        u_d1.append(int(float(p[0])))
-                    if count == 7 or count ==8:
-                        u_s2.append(int(float(p[0])))
-                    if count == 9 or count ==10:
-                        u_d2.append(int(float(p[0])))
-                    if count == 11:
-                        R = int(float(p[0]))
-            if count>11:
-                if len(s.split())!=4:
-                    return []
-                else:
-                    nombres = [int(float(x)) for x in p]
-                    obs.append(nombres)
-            count +=1
-        return [xmax,ymax,u_s1,u_d1, u_s2, u_d2, R, obs]
-
-print(valid_input())
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -105,5 +63,7 @@ def plot_environment():
     plt.tight_layout()
     return fig, ax
 
-fig, ax = plot_environment()
-plt.show()
+
+def main():        
+    fig, ax = plot_environment()
+    plt.show()
